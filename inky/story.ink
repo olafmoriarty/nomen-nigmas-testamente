c2b9271e0cc50dde0e469952f9f9f7b53221e600
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2022-02-20 21:12:40 +0100

    Add Ink support and implement continue/choices

---
+++ inky/story.ink
@@ -0,0 +1,27 @@
+En bussjåfør! #stil:dialog #person:hamilton
+
+En bussjåfør, Sir? #person:burr
+
+Det er en mann med godt humør. #person:hamilton
+
+Sure... #person:burr
+
+Men har han ikke godt humør?
+
+Hør; #person:hamilton
+
+Da er han ingen bussjåfør.
+
+Kjør. #person:burr
+
++ Kjør til venstre
++ Kjør til høgre
++ Kjør rett fram
+
+- Kva er det du gjør?
+
+Det snør det snør
+
+Det er det det gjør, Sir
+
+    -> END
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './css/game.css';
+import { Story } from 'inkjs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inkStory, setInkStory] = useState(false);
+	const [currentText, setCurrentText] = useState(false);
+
+	useEffect(() => {
+		const json = require('./story.json');
+		const ink = new Story(json);
+		setInkStory(ink);
+	}, []);
+	
+	const progress = () => {
+		if (inkStory.canContinue) {
+			const text = inkStory.Continue();
+			const textObject = {
+				...currentText,
+				choices: [],
+				text: text,
+			}
+			inkStory.currentTags.forEach(element => {
+				const tag = element.split(':');
+				textObject[tag[0]] = tag[1];
+			});
+			console.log(textObject);
+			setCurrentText(textObject);
+		}
+		else if (inkStory.currentChoices.length > 0) {
+			const textObject = {
+				...currentText,
+				choices: inkStory.currentChoices,
+			}
+			console.log(textObject.choices);
+			setCurrentText(textObject);
+		}
+	}
+
+	const makeChoice = choice => {
+		inkStory.ChooseChoiceIndex(choice);
+		progress();
+	}
+
+	return (
+		<>
+			{currentText.text ? <div className="story-line">
+				{currentText.person ? <div className="speaker-name">{currentText.person}</div> : false}
+				<div className="text">{currentText.text}</div>
+			</div> : false}
+			{(!currentText.choices || !currentText.choices.length) ? <button className="progress-button" onClick={() => progress()}></button> : false}
+			{(currentText.choices && currentText.choices.length) ? <div className="choices">
+				{currentText.choices.map((el, index) => <button key={index} className="option" onClick={() => makeChoice(index)}>{el.text}</button>)}
+			</div> : false}
+		</>
+	)
 }
 
-export default App;
+export default App
